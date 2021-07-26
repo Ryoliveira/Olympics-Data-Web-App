4e830521c9af02957544d1f1c64b928184f24d25
Ryoliveira <windsofryannn@yahoo.com>
2021-07-25 17:30:57 -0700

    Initial Commit

---
+++ src/main/java/com/ryoliveira/olympicmedaldisplay/OlympicMedalDisplayApplication.java
@@ -0,0 +1,13 @@
+package com.ryoliveira.olympicmedaldisplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlympicMedalDisplayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlympicMedalDisplayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ryoliveira/olympicmedaldisplay/OlympicMedalDisplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryoliveira.olympicmedaldisplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlympicMedalDisplayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
