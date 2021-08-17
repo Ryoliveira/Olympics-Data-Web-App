35251aac5c97e2068473420ee64d2eaa18f2abfd
Ryoliveira <windsofryannn@yahoo.com>
2021-08-16 20:47:11 -0700

    Changed all DataController return types to ResponseEntities

--- src/main/java/com/ryoliveira/olympicmedaldisplay/controller/DataController.java
+++ src/main/java/com/ryoliveira/olympicmedaldisplay/controller/DataController.java
@@ -3,6 +3,7 @@ package com.ryoliveira.olympicmedaldisplay.controller;
 import com.ryoliveira.olympicmedaldisplay.model.*;
 import com.ryoliveira.olympicmedaldisplay.service.*;
 import org.springframework.data.domain.*;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,7 +16,7 @@ public class DataController {
     private final CountryService countryService;
 
     public DataController(AthleteService athleteService, TeamService teamService,
-                          SportService sportService, CountryService countryService){
+                          SportService sportService, CountryService countryService) {
         this.athleteService = athleteService;
         this.teamService = teamService;
         this.sportService = sportService;
@@ -23,56 +24,54 @@ public class DataController {
     }
 
     @GetMapping("/{sport}/standings")
-    public TeamList getStandings(@PathVariable String sport){
-        return this.teamService.getAllTeamStandings(sport);
+    public ResponseEntity<TeamList> getStandings(@PathVariable String sport) {
+        return new ResponseEntity<>(this.teamService.getAllTeamStandings(sport), HttpStatus.OK);
     }
 
     @GetMapping("/sports")
-    public SportsList getSportsList(){
-        return this.sportService.getSportList();
+    public ResponseEntity<SportsList> getSportsList() {
+        return new ResponseEntity<>(this.sportService.getSportList(), HttpStatus.OK);
     }
 
     @GetMapping("/athletes")
-    public Page<Athlete> getAllAthletes(@RequestParam("page") int page, @RequestParam("size") int size){
-        return this.athleteService.getAllAthletes(page, size);
+    public ResponseEntity<Page<Athlete>> getAllAthletes(@RequestParam("page") int page, @RequestParam("size") int size) {
+        return new ResponseEntity<>(this.athleteService.getAllAthletes(page, size), HttpStatus.OK);
     }
 
     @GetMapping("/team/{country}/athletes")
-    public Page<Athlete> getAllAthletesByCountry(@PathVariable String country, @RequestParam("page") int page, @RequestParam("size") int size){
-        return this.athleteService.getAllAthletesByCountry(country, page, size);
+    public ResponseEntity<Page<Athlete>> getAllAthletesByCountry(@PathVariable String country, @RequestParam("page") int page, @RequestParam("size") int size) {
+        return new ResponseEntity<>(this.athleteService.getAllAthletesByCountry(country, page, size), HttpStatus.FOUND);
     }
 
     @GetMapping("/{sport}/athletes")
-    public Page<Athlete> getAllAthletesByDiscipline(@PathVariable String sport, @RequestParam("page") int page, @RequestParam("size") int size){
-        return this.athleteService.getAllAthletesByDiscipline(sport, page, size);
+    public ResponseEntity<Page<Athlete>> getAllAthletesByDiscipline(@PathVariable String sport, @RequestParam("page") int page, @RequestParam("size") int size) {
+        return new ResponseEntity<>(this.athleteService.getAllAthletesByDiscipline(sport, page, size), HttpStatus.FOUND);
     }
 
     @GetMapping("/athlete/{name}")
-    public Athlete getAthleteByName(@PathVariable String name){
-        return this.athleteService.getAthleteByName(name);
+    public ResponseEntity<Athlete> getAthleteByName(@PathVariable String name) {
+        return new ResponseEntity<>(this.athleteService.getAthleteByName(name), HttpStatus.FOUND);
     }
 
     @GetMapping("/{sport}/{country}/athletes")
-    public Page<Athlete> getAthletesBySportAndCountry(@PathVariable("sport") String sport, @PathVariable("country") String country,
-                                                      @RequestParam("page") int pageNumber, @RequestParam("size") int pageSize){
-        return this.athleteService.getAthletesBySportAndCountry(sport, country, pageNumber, pageSize);
+    public ResponseEntity<Page<Athlete>> getAthletesBySportAndCountry(@PathVariable("sport") String sport, @PathVariable("country") String country,
+                                                                      @RequestParam("page") int pageNumber, @RequestParam("size") int pageSize) {
+        return new ResponseEntity<>(this.athleteService.getAthletesBySportAndCountry(sport, country, pageNumber, pageSize), HttpStatus.FOUND);
     }
 
     @GetMapping("/countries")
-    public CountryList getAllCountries(){
-        return this.countryService.getCountryList();
+    public ResponseEntity<CountryList> getAllCountries() {
+        return new ResponseEntity<>(this.countryService.getCountryList(), HttpStatus.OK);
     }
 
     @GetMapping("/{sport}/information")
-    public SportInformation getSportInformation(@PathVariable String sport){
-        return this.sportService.getSportInformation(sport);
+    public ResponseEntity<SportInformation> getSportInformation(@PathVariable String sport) {
+        return new ResponseEntity<>(this.sportService.getSportInformation(sport), HttpStatus.FOUND);
     }
 
     @GetMapping("/information/{country}")
-    public CountryInformation getCountryInformation(@PathVariable String country){
-        return this.countryService.getCountryInformation(country);
+    public ResponseEntity<CountryInformation> getCountryInformation(@PathVariable String country) {
+        return new ResponseEntity<>(this.countryService.getCountryInformation(country), HttpStatus.FOUND);
     }
-
-
 
 }
